18642a77e6e5f290da948acecbcea76f00d92370
earine <marinalunts@gmail.com>
2019-05-20 20:09:51 +0300

    Initial Commit

---
+++ Plantify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Plantify
+//
+//  Created by Marina Lunts on 5/20/19.
+//  Copyright © 2019 earine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
